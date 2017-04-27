7df1ba7911e6fbf6ff0b56153b00b945aae52dd5
MortizDX <maortiz4@uc.cl>
2017-04-26 22:56:06 -0300

    fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :full_name :email :username, presence: true, allow_blank: false,format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  validates :email, presence: true, allow_blank: false,format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates :password, presence: true, confirmation: true, allow_blank: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,9 @@ ActiveRecord::Schema.define(version: 20170426200336) do
 
   create_table "users", force: :cascade do |t|
     t.string   "full_name"
-    t.string   "email"
-    t.string   "username"
-    t.string   "password"
+    t.string   "email",      null: false
+    t.string   "username",   null: false
+    t.string   "password",   null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
