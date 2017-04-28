7a2f5721c0527086fbf9026bc7eb8943249a95ed
isarovic <isarovic@uc.cl>
2017-04-28 15:01:01 -0300

    prueba esta

---
+++ AppPrueba/app/controllers/welcome_controller.rb
@@ -0,0 +1,7 @@
+# CREAR CONTROLADOR CON UNA ACCION INDEX: 
+#                 bin/rails generate controller Welcome index
+#                 -> genera el controlador (lo de abajo) y un archivo html asociado, que es la vista
+class WelcomeController < ApplicationController
+  def index
+  end
+end
