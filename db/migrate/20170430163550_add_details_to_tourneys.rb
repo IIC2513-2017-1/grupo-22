25e7c3aee32a80b32edf42589246b59e661efbf1
isarovic <isarovic@uc.cl>
2017-04-30 13:39:42 -0300

    probando

---
+++ db/migrate/20170430163550_add_details_to_tourneys.rb
@@ -0,0 +1,9 @@
+class AddDetailsToTourneys < ActiveRecord::Migration[5.0]
+  def change
+    add_column :tourneys, :schedule, :string
+    add_column :tourneys, :prize, :text
+    add_column :tourneys, :price, :string
+    add_column :tourneys, :mail, :string
+    add_column :tourneys, :phone, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429210922) do
+ActiveRecord::Schema.define(version: 20170430163550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,11 @@ ActiveRecord::Schema.define(version: 20170429210922) do
     t.integer  "user_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "schedule"
+    t.text     "prize"
+    t.string   "price"
+    t.string   "mail"
+    t.string   "phone"
     t.index ["user_id"], name: "index_tourneys_on_user_id", using: :btree
   end
 
