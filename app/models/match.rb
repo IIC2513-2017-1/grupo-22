4b655ec6d289b0f81d9ab17cc9cbf92c5a0e8a82
isarovic <isarovic@uc.cl>
2017-04-30 22:05:27 -0300

    arreglos

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,7 +1,7 @@
 class Match < ApplicationRecord
   include CheckTeam
   validate :search_home_team
-  validte :search_away_team
+  validate :search_away_team
 
   belongs_to :tourney
   belongs_to :home_team, foreign_key: 'home_team_id', class_name: 'Team'
