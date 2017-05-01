af2d1bc97c5f76e5640eb605ca5ec3bf0247b263
isarovic <isarovic@uc.cl>
2017-04-30 22:44:16 -0300

    arreglos 3

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,6 @@
 class Team < ApplicationRecord
   validates :name, presence: true, allow_blank: false, uniqueness: true
   validates :description, presence: true, allow_blank: false
-  validates :logo, presence: true, allow_blank: false 
-  validates :trainer, presence: true, allow_blank: false
     
   has_many :players, dependent: :destroy
   belongs_to :user
