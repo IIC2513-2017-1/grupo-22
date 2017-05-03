fc1cd3dfe1ebfd6e9ad63f01a7c61982d379f20a
isarovic <isarovic@uc.cl>
2017-05-02 21:04:12 -0300

    vista aplicattion detalles visuales

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 	 end
 
 	 def create
-	    	user = User.find_by(username: params[:session][:username])
+	     user = User.find_by(username: params[:session][:username])
 	     if user&.authenticate(params[:session][:password])
 	     	session[:user_id] = user.id
 	    	redirect_to root_path, notice: 'Login successful.'
