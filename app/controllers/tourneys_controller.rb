626d59bff7f266a745017f97574b79d3d563becc
MortizDX <maortiz4@uc.cl>
2017-05-09 23:55:41 -0300

    Agregar equipos a torneos (muy básico)

--- app/controllers/tourneys_controller.rb
+++ app/controllers/tourneys_controller.rb
@@ -67,6 +67,13 @@ class TourneysController < ApplicationController
     end
   end
 
+  def add_team
+    @team = Team.find(params[:team_id])
+    @tourney = Tourney.find(params[:tourney_id])
+    @tourney.teams << @team
+    redirect_to tourney_path(@tourney)
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_tourney
