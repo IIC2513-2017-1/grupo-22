22b014a84b8e7bd7dde82736e3703edcc67435eb
isarovic <isarovic@uc.cl>
2017-06-03 00:06:58 -0400

    modals sign up y creacion teams everywhere

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function(){
+
+	$(document).ready(function(){
+
+		var buttonAddTeam = $(".add-team");
+
+		var modalBackground = $(".modal-background.team");
+
+		var buttonClose = $(".close");
+
+		buttonAddTeam.on("click", function(e){		
+			modalBackground.fadeIn("slow");
+			e.preventDefault();	
+		});
+
+		buttonClose.on("click", function(){
+			modalBackground.fadeOut("slow");
+		});
+	});
+});
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,23 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function(){
+
+	$(document).ready(function(){
+
+		var buttonSignUp= $("#sign-up");
+
+		var modalBackground = $(".modal-background.sign-up");
+
+		var buttonClose = $(".close");
+
+		buttonSignUp.on("click", function(e){		
+			modalBackground.fadeIn("slow");
+			e.preventDefault();	
+		});
+
+		buttonClose.on("click", function(){
+			modalBackground.fadeOut("slow");
+		});
+	});
+});
