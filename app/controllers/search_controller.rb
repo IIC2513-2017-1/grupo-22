f9b0d5899fcb4eb2ad0d74e87d06338daa3fb75d
MortizDX <maortiz4@uc.cl>
2017-07-01 01:40:00 -0400

    Players al search, arreglos menores

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,11 @@ class SearchController < ApplicationController
     if params[:search]
       @teams = Team.search(params[:search]).order("name DESC")
       @tourneys = Tourney.search(params[:search]).order("name DESC")
+      @players = Player.search(params[:search]).order("full_name DESC")
     else
-      @teams = Team.all.order("name_at DESC")
-      @tourneys = Tourney.all.order("name_at DESC")
+      @teams = Team.all.order("name DESC")
+      @tourneys = Tourney.all.order("name DESC")
+      @players = Player.all.order("full_name DESC")
     end
   end
 end
